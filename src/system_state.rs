f46d0ffe240329a938c87c3e522a87b1e7aaaa74
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2026-02-22 16:36:42 +0100

    two space

--- src/system_state.rs
+++ src/system_state.rs
@@ -1,7 +1,7 @@
 use crate::battery::{
-    ACPIType, BatteryStates, BatteryStatesError, ChargingStatus, PlatformProfile,
+  ACPIType, BatteryStates, BatteryStatesError, ChargingStatus, PlatformProfile,
 };
-use crate::cpu::{CpuStates, CpuStatesError, CpuType, ScalingGoverner, EPP};
+use crate::cpu::{CpuStates, CpuStatesError, CpuType, EPP, ScalingGoverner};
 use std::cell::RefCell;
 use std::fmt;
 use std::fs;
@@ -10,287 +10,293 @@ use std::path::Path;
 
 #[derive(Debug)]
 pub enum SystemStateError {
-    ACPITypeErr(String),
+  ACPITypeErr(String),
 
-    CpuStatesErr(CpuStatesError),
-    BatteryStatesErr(BatteryStatesError),
-    GeneralIoErr(io::Error),
+  CpuStatesErr(CpuStatesError),
+  BatteryStatesErr(BatteryStatesError),
+  GeneralIoErr(io::Error),
 }
 
 impl fmt::Display for SystemStateError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            SystemStateError::ACPITypeErr(e) => write!(f, "{e}"),
-            SystemStateError::CpuStatesErr(e) => write!(f, "{e}"),
-            SystemStateError::BatteryStatesErr(e) => write!(f, "{e}"),
-            SystemStateError::GeneralIoErr(e) => write!(f, "General io error: {e}"),
-        }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      SystemStateError::ACPITypeErr(e) => write!(f, "{e}"),
+      SystemStateError::CpuStatesErr(e) => write!(f, "{e}"),
+      SystemStateError::BatteryStatesErr(e) => write!(f, "{e}"),
+      SystemStateError::GeneralIoErr(e) => write!(f, "General io error: {e}"),
     }
+  }
 }
 
 impl From<CpuStatesError> for SystemStateError {
-    fn from(error: CpuStatesError) -> Self {
-        SystemStateError::CpuStatesErr(error)
-    }
+  fn from(error: CpuStatesError) -> Self {
+    SystemStateError::CpuStatesErr(error)
+  }
 }
 
 impl From<BatteryStatesError> for SystemStateError {
-    fn from(error: BatteryStatesError) -> Self {
-        SystemStateError::BatteryStatesErr(error)
-    }
+  fn from(error: BatteryStatesError) -> Self {
+    SystemStateError::BatteryStatesErr(error)
+  }
 }
 
 impl From<io::Error> for SystemStateError {
-    fn from(error: io::Error) -> Self {
-        SystemStateError::GeneralIoErr(error)
-    }
+  fn from(error: io::Error) -> Self {
+    SystemStateError::GeneralIoErr(error)
+  }
 }
 
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum State {
-    Powersave,
-    Balanced,
-    Performance,
+  Powersave,
+  Balanced,
+  Performance,
 }
 
 pub struct SystemState {
-    pub linux: bool,
-    pub cpu_type: CpuType,
-    pub acpi_type: ACPIType,
+  pub linux: bool,
+  pub cpu_type: CpuType,
+  pub acpi_type: ACPIType,
 
-    pub cpu_states: CpuStates,
-    pub battery_states: BatteryStates,
+  pub cpu_states: CpuStates,
+  pub battery_states: BatteryStates,
 
-    pub state: RefCell<State>,
+  pub state: RefCell<State>,
 }
 
 impl fmt::Display for SystemState {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}\n{}\nSystem state: {:?}",
-            self.cpu_states,
-            self.battery_states,
-            *self.state.borrow(),
-        )
-    }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(
+      f,
+      "{}\n{}\nSystem state: {:?}",
+      self.cpu_states,
+      self.battery_states,
+      *self.state.borrow(),
+    )
+  }
 }
 
 impl SystemState {
-    pub fn init() -> Result<Self, SystemStateError> {
-        let cpu_type = Self::detect_cpu_type();
-        let cpu_states = CpuStates::init(Self::num_cpu_cores()?, &cpu_type)?;
-        Ok(Self {
-            linux: Self::detect_linux(),
-            cpu_type,
-            acpi_type: Self::detect_acpi_type(),
+  pub fn init() -> Result<Self, SystemStateError> {
+    let cpu_type = Self::detect_cpu_type();
+    let cpu_states = CpuStates::init(Self::num_cpu_cores()?, &cpu_type)?;
+    Ok(Self {
+      linux: Self::detect_linux(),
+      cpu_type,
+      acpi_type: Self::detect_acpi_type(),
 
-            cpu_states,
-            battery_states: BatteryStates::init()?,
+      cpu_states,
+      battery_states: BatteryStates::init()?,
 
-            state: RefCell::new(State::Powersave),
-        })
+      state: RefCell::new(State::Powersave),
+    })
+  }
+
+  pub fn post_init(&self) -> Result<(), SystemStateError> {
+    match self.battery_states.read_charging_status()? {
+      ChargingStatus::Charging | ChargingStatus::NotCharging => {
+        self.set_performance_mode(false)?;
+        *self.state.borrow_mut() = State::Performance;
+      }
+      ChargingStatus::DisCharging => {
+        self.set_powersave_mode()?;
+        *self.state.borrow_mut() = State::Powersave;
+      }
+      ChargingStatus::Unknown => {
+        self.set_balanced_mode()?;
+        *self.state.borrow_mut() = State::Powersave;
+      }
     }
 
-    pub fn post_init(&self) -> Result<(), SystemStateError> {
-        match self.battery_states.read_charging_status()? {
-            ChargingStatus::Charging | ChargingStatus::NotCharging => {
-                self.set_performance_mode(false)?;
-                *self.state.borrow_mut() = State::Performance;
-            }
-            ChargingStatus::DisCharging => {
-                self.set_powersave_mode()?;
-                *self.state.borrow_mut() = State::Powersave;
-            }
-            ChargingStatus::Unknown => {
-                self.set_balanced_mode()?;
-                *self.state.borrow_mut() = State::Powersave;
-            }
+    Ok(())
+  }
+
+  pub fn set_powersave_mode(&self) -> Result<(), SystemStateError> {
+    self
+      .cpu_states
+      .set_scaling_governer(ScalingGoverner::Powersave)?;
+
+    self.cpu_states.set_epp(EPP::Power)?;
+
+    self
+      .battery_states
+      .set_platform_profile(&PlatformProfile::LowPower)?;
+
+    self.cpu_states.set_cpu_boost(false)?;
+
+    Ok(())
+  }
+
+  // for now, only for high cpu temp situations when charging
+  pub fn set_balanced_mode(&self) -> Result<(), SystemStateError> {
+    self
+      .cpu_states
+      .set_scaling_governer(ScalingGoverner::Powersave)?;
+
+    self.cpu_states.set_epp(EPP::BalancePower)?;
+
+    self
+      .battery_states
+      .set_platform_profile(&PlatformProfile::Balanced)?;
+
+    self.cpu_states.set_cpu_boost(false)?;
+
+    Ok(())
+  }
+
+  pub fn set_performance_mode(&self, cpu_boost: bool) -> Result<(), SystemStateError> {
+    if self.battery_states.read_charging_status()? == ChargingStatus::DisCharging {
+      return Ok(());
+    }
+
+    self
+      .cpu_states
+      .set_scaling_governer(ScalingGoverner::Performance)?;
+
+    self.cpu_states.set_epp(EPP::Performance)?;
+
+    self
+      .battery_states
+      .set_platform_profile(&PlatformProfile::Performance)?;
+
+    self.cpu_states.set_cpu_boost(cpu_boost)?;
+
+    Ok(())
+  }
+
+  // TODO: look over all of these again bc it's vibe coded
+  fn detect_linux() -> bool {
+    #[cfg(target_os = "linux")]
+    let compile_time = true;
+    #[cfg(not(target_os = "linux"))]
+    let compile_time = false;
+
+    let runtime_uname = std::process::Command::new("uname")
+      .arg("-s")
+      .output()
+      .ok()
+      .and_then(|o| String::from_utf8(o.stdout).ok())
+      .map(|s| s.trim().eq_ignore_ascii_case("linux"))
+      .unwrap_or(false);
+
+    let has_proc = Path::new("/proc").exists();
+    let has_sys = Path::new("/sys").exists();
+    let has_etc = Path::new("/etc").exists();
+
+    let has_os_release =
+      Path::new("/etc/os-release").exists() || Path::new("/usr/lib/os-release").exists();
+
+    compile_time
+      || (runtime_uname && has_proc && has_sys)
+      || (has_proc && has_sys && has_etc && has_os_release)
+  }
+
+  fn detect_cpu_type() -> CpuType {
+    if let Ok(cpuinfo) = fs::read_to_string("/proc/cpuinfo") {
+      for line in cpuinfo.lines() {
+        if line.starts_with("vendor_id") {
+          if line.contains("GenuineIntel") {
+            return CpuType::Intel;
+          } else if line.contains("AuthenticAMD") {
+            return CpuType::AMD;
+          }
         }
-
-        Ok(())
-    }
-
-    pub fn set_powersave_mode(&self) -> Result<(), SystemStateError> {
-        self.cpu_states
-            .set_scaling_governer(ScalingGoverner::Powersave)?;
-
-        self.cpu_states.set_epp(EPP::Power)?;
-
-        self.battery_states
-            .set_platform_profile(&PlatformProfile::LowPower)?;
-
-        self.cpu_states.set_cpu_boost(false)?;
-
-        Ok(())
-    }
-
-    // for now, only for high cpu temp situations when charging
-    pub fn set_balanced_mode(&self) -> Result<(), SystemStateError> {
-        self.cpu_states
-            .set_scaling_governer(ScalingGoverner::Powersave)?;
-
-        self.cpu_states.set_epp(EPP::BalancePower)?;
-
-        self.battery_states
-            .set_platform_profile(&PlatformProfile::Balanced)?;
-
-        self.cpu_states.set_cpu_boost(false)?;
-
-        Ok(())
-    }
-
-    pub fn set_performance_mode(&self, cpu_boost: bool) -> Result<(), SystemStateError> {
-        if self.battery_states.read_charging_status()? == ChargingStatus::DisCharging {
-            return Ok(());
-        }
-
-        self.cpu_states
-            .set_scaling_governer(ScalingGoverner::Performance)?;
-
-        self.cpu_states.set_epp(EPP::Performance)?;
-
-        self.battery_states
-            .set_platform_profile(&PlatformProfile::Performance)?;
-
-        self.cpu_states.set_cpu_boost(cpu_boost)?;
-
-        Ok(())
-    }
-
-    // TODO: look over all of these again bc it's vibe coded
-    fn detect_linux() -> bool {
-        #[cfg(target_os = "linux")]
-        let compile_time = true;
-        #[cfg(not(target_os = "linux"))]
-        let compile_time = false;
-
-        let runtime_uname = std::process::Command::new("uname")
-            .arg("-s")
-            .output()
-            .ok()
-            .and_then(|o| String::from_utf8(o.stdout).ok())
-            .map(|s| s.trim().eq_ignore_ascii_case("linux"))
-            .unwrap_or(false);
-
-        let has_proc = Path::new("/proc").exists();
-        let has_sys = Path::new("/sys").exists();
-        let has_etc = Path::new("/etc").exists();
-
-        let has_os_release =
-            Path::new("/etc/os-release").exists() || Path::new("/usr/lib/os-release").exists();
-
-        compile_time
-            || (runtime_uname && has_proc && has_sys)
-            || (has_proc && has_sys && has_etc && has_os_release)
-    }
-
-    fn detect_cpu_type() -> CpuType {
-        if let Ok(cpuinfo) = fs::read_to_string("/proc/cpuinfo") {
-            for line in cpuinfo.lines() {
-                if line.starts_with("vendor_id") {
-                    if line.contains("GenuineIntel") {
-                        return CpuType::Intel;
-                    } else if line.contains("AuthenticAMD") {
-                        return CpuType::AMD;
-                    }
-                }
-            }
-        }
-
-        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-        {
-            if let Some(cpu_type) = Self::detect_cpu_via_cpuid() {
-                return cpu_type;
-            }
-        }
-
-        if let Ok(output) = std::process::Command::new("lscpu").output() {
-            if let Ok(text) = String::from_utf8(output.stdout) {
-                let lower = text.to_lowercase();
-                if lower.contains("genuineintel") || lower.contains("intel") {
-                    return CpuType::Intel;
-                } else if lower.contains("authenticamd") || lower.contains("amd") {
-                    return CpuType::AMD;
-                }
-            }
-        }
-
-        CpuType::Unknown
+      }
     }
 
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-    fn detect_cpu_via_cpuid() -> Option<CpuType> {
-        #[cfg(target_arch = "x86")]
-        use core::arch::x86::__cpuid;
-        #[cfg(target_arch = "x86_64")]
-        use core::arch::x86_64::__cpuid;
-
-        unsafe {
-            // CPUID leaf 0 returns vendor string in EBX, EDX, ECX
-            let result = __cpuid(0);
-
-            // convert registers to bytes
-            let mut vendor = [0u8; 12];
-            vendor[0..4].copy_from_slice(&result.ebx.to_le_bytes());
-            vendor[4..8].copy_from_slice(&result.edx.to_le_bytes());
-            vendor[8..12].copy_from_slice(&result.ecx.to_le_bytes());
-
-            match &vendor {
-                b"GenuineIntel" => Some(CpuType::Intel),
-                b"AuthenticAMD" => Some(CpuType::AMD),
-                _ => None,
-            }
-        }
+    {
+      if let Some(cpu_type) = Self::detect_cpu_via_cpuid() {
+        return cpu_type;
+      }
     }
 
-    fn num_cpu_cores() -> Result<usize, SystemStateError> {
-        let cpu_dir = "/sys/devices/system/cpu/";
-        let mut count = 0;
-
-        for entry in fs::read_dir(cpu_dir)? {
-            let entry = entry?;
-            let path = entry.path();
-
-            if path.is_dir() {
-                if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
-                    if name.starts_with("cpu") && name != "cpufreq" {
-                        // Check if the remaining part of the name is a number
-                        if name[3..].parse::<u32>().is_ok() {
-                            count += 1;
-                        }
-                    }
-                }
-            }
+    if let Ok(output) = std::process::Command::new("lscpu").output() {
+      if let Ok(text) = String::from_utf8(output.stdout) {
+        let lower = text.to_lowercase();
+        if lower.contains("genuineintel") || lower.contains("intel") {
+          return CpuType::Intel;
+        } else if lower.contains("authenticamd") || lower.contains("amd") {
+          return CpuType::AMD;
         }
-        Ok(count)
+      }
     }
 
-    fn detect_acpi_type() -> ACPIType {
-        if let Ok(product_version) = fs::read_to_string("/sys/class/dmi/id/product_version") {
-            let product_version = product_version.trim().to_lowercase();
-            if product_version.contains("thinkpad") {
-                return ACPIType::ThinkPad;
-            }
-            //if product_version.contains("ideapad") {
-            //    return ACPIType::IdeaPad;
-            //}
-        }
+    CpuType::Unknown
+  }
 
-        if let Ok(product_name) = fs::read_to_string("/sys/class/dmi/id/product_name") {
-            let product_name = product_name.trim().to_lowercase();
-            if product_name.contains("thinkpad") {
-                return ACPIType::ThinkPad;
-            }
-            //if product_name.contains("ideapad") {
-            //    return ACPIType::IdeaPad;
-            //}
-        }
+  #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+  fn detect_cpu_via_cpuid() -> Option<CpuType> {
+    #[cfg(target_arch = "x86")]
+    use core::arch::x86::__cpuid;
+    #[cfg(target_arch = "x86_64")]
+    use core::arch::x86_64::__cpuid;
 
-        if Path::new("/proc/acpi/ibm").exists() {
-            return ACPIType::ThinkPad;
-        }
+    unsafe {
+      // CPUID leaf 0 returns vendor string in EBX, EDX, ECX
+      let result = __cpuid(0);
 
-        ACPIType::Unknown
+      // convert registers to bytes
+      let mut vendor = [0u8; 12];
+      vendor[0..4].copy_from_slice(&result.ebx.to_le_bytes());
+      vendor[4..8].copy_from_slice(&result.edx.to_le_bytes());
+      vendor[8..12].copy_from_slice(&result.ecx.to_le_bytes());
+
+      match &vendor {
+        b"GenuineIntel" => Some(CpuType::Intel),
+        b"AuthenticAMD" => Some(CpuType::AMD),
+        _ => None,
+      }
     }
+  }
+
+  fn num_cpu_cores() -> Result<usize, SystemStateError> {
+    let cpu_dir = "/sys/devices/system/cpu/";
+    let mut count = 0;
+
+    for entry in fs::read_dir(cpu_dir)? {
+      let entry = entry?;
+      let path = entry.path();
+
+      if path.is_dir() {
+        if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
+          if name.starts_with("cpu") && name != "cpufreq" {
+            // Check if the remaining part of the name is a number
+            if name[3..].parse::<u32>().is_ok() {
+              count += 1;
+            }
+          }
+        }
+      }
+    }
+    Ok(count)
+  }
+
+  fn detect_acpi_type() -> ACPIType {
+    if let Ok(product_version) = fs::read_to_string("/sys/class/dmi/id/product_version") {
+      let product_version = product_version.trim().to_lowercase();
+      if product_version.contains("thinkpad") {
+        return ACPIType::ThinkPad;
+      }
+      //if product_version.contains("ideapad") {
+      //    return ACPIType::IdeaPad;
+      //}
+    }
+
+    if let Ok(product_name) = fs::read_to_string("/sys/class/dmi/id/product_name") {
+      let product_name = product_name.trim().to_lowercase();
+      if product_name.contains("thinkpad") {
+        return ACPIType::ThinkPad;
+      }
+      //if product_name.contains("ideapad") {
+      //    return ACPIType::IdeaPad;
+      //}
+    }
+
+    if Path::new("/proc/acpi/ibm").exists() {
+      return ACPIType::ThinkPad;
+    }
+
+    ACPIType::Unknown
+  }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -5,175 +5,179 @@ use std::env;
 use std::fmt;
 use std::fs;
 use std::fs::{File, OpenOptions};
-use std::io::{self, prelude::*, Error, ErrorKind, Seek, SeekFrom, Write};
+use std::io::{self, Error, ErrorKind, Seek, SeekFrom, Write, prelude::*};
 use std::path::Path;
 
 #[derive(Debug)]
 pub enum PersFdError {
-    InvalidFilePerms,
-    ReadErr(io::Error),
-    WriteErr(io::Error),
+  InvalidFilePerms,
+  ReadErr(io::Error),
+  WriteErr(io::Error),
 }
 
 impl fmt::Display for PersFdError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            PersFdError::InvalidFilePerms => write!(f, "Invalid file permissions"),
-            PersFdError::ReadErr(e) => write!(f, "Read error: {e}"),
-            PersFdError::WriteErr(e) => write!(f, "Write error: {e}"),
-        }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      PersFdError::InvalidFilePerms => write!(f, "Invalid file permissions"),
+      PersFdError::ReadErr(e) => write!(f, "Read error: {e}"),
+      PersFdError::WriteErr(e) => write!(f, "Write error: {e}"),
     }
+  }
 }
 
 pub struct PersFd {
-    file: File,
-    write: bool,
-    //path: String,
+  file: File,
+  write: bool,
+  //path: String,
 }
 
 impl PersFd {
-    pub fn new(path: &str, write: bool) -> Result<Self, PersFdError> {
-        let file = OpenOptions::new()
-            .read(true)
-            .write(write)
-            .open(path)
-            .map_err(PersFdError::ReadErr)?;
+  pub fn new(path: &str, write: bool) -> Result<Self, PersFdError> {
+    let file = OpenOptions::new()
+      .read(true)
+      .write(write)
+      .open(path)
+      .map_err(PersFdError::ReadErr)?;
 
-        Ok(PersFd { file, write })
+    Ok(PersFd { file, write })
+  }
+
+  pub fn read_value(&mut self) -> Result<String, PersFdError> {
+    self
+      .file
+      .seek(SeekFrom::Start(0))
+      .map_err(PersFdError::ReadErr)?;
+    let mut contents = String::new();
+    self
+      .file
+      .read_to_string(&mut contents)
+      .map_err(PersFdError::ReadErr)?;
+    Ok(contents.trim().to_string())
+  }
+
+  pub fn set_value(&mut self, value: &str) -> Result<(), PersFdError> {
+    if !self.write {
+      return Err(PersFdError::InvalidFilePerms);
     }
 
-    pub fn read_value(&mut self) -> Result<String, PersFdError> {
-        self.file
-            .seek(SeekFrom::Start(0))
-            .map_err(PersFdError::ReadErr)?;
-        let mut contents = String::new();
-        self.file
-            .read_to_string(&mut contents)
-            .map_err(PersFdError::ReadErr)?;
-        Ok(contents.trim().to_string())
-    }
-
-    pub fn set_value(&mut self, value: &str) -> Result<(), PersFdError> {
-        if !self.write {
-            return Err(PersFdError::InvalidFilePerms);
-        }
-
-        self.file
-            .seek(io::SeekFrom::Start(0))
-            .map_err(PersFdError::WriteErr)?;
-        self.file.set_len(0).map_err(PersFdError::WriteErr)?;
-        self.file
-            .write_all(format!("{}\n", value).as_bytes())
-            .map_err(PersFdError::WriteErr)?;
-        self.file.flush().map_err(PersFdError::WriteErr)
-    }
+    self
+      .file
+      .seek(io::SeekFrom::Start(0))
+      .map_err(PersFdError::WriteErr)?;
+    self.file.set_len(0).map_err(PersFdError::WriteErr)?;
+    self
+      .file
+      .write_all(format!("{}\n", value).as_bytes())
+      .map_err(PersFdError::WriteErr)?;
+    self.file.flush().map_err(PersFdError::WriteErr)
+  }
 }
 
 #[allow(dead_code)]
 pub trait StyledString {
-    fn red(&self) -> String;
-    fn green(&self) -> String;
-    fn yellow(&self) -> String;
+  fn red(&self) -> String;
+  fn green(&self) -> String;
+  fn yellow(&self) -> String;
 }
 
 impl StyledString for str {
-    fn red(&self) -> String {
-        format!("\x1b[31m{}\x1b[0m", self)
-    }
+  fn red(&self) -> String {
+    format!("\x1b[31m{}\x1b[0m", self)
+  }
 
-    fn green(&self) -> String {
-        format!("\x1b[32m{}\x1b[0m", self)
-    }
+  fn green(&self) -> String {
+    format!("\x1b[32m{}\x1b[0m", self)
+  }
 
-    fn yellow(&self) -> String {
-        format!("\x1b[33m{}\x1b[0m", self)
-    }
+  fn yellow(&self) -> String {
+    format!("\x1b[33m{}\x1b[0m", self)
+  }
 }
 
 #[derive(Deserialize)]
 struct ConfigFile {
-    battery: BatteryConfig,
+  battery: BatteryConfig,
 }
 
 #[derive(Deserialize)]
 struct BatteryConfig {
-    start_threshold: u8,
-    stop_threshold: u8,
+  start_threshold: u8,
+  stop_threshold: u8,
 }
 
 pub struct Config {
-    pub charge_start_threshold: Option<u8>,
-    pub charge_stop_threshold: Option<u8>,
+  pub charge_start_threshold: Option<u8>,
+  pub charge_stop_threshold: Option<u8>,
 }
 
 impl fmt::Display for Config {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "SystemFds Read:")
-    }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "SystemFds Read:")
+  }
 }
 
 impl Config {
-    pub fn parse(config_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        if !Path::new(config_path).exists() {
-            return Err(Box::new(Error::new(
-                ErrorKind::NotFound,
-                "No config file found",
-            )));
-        }
-
-        let contents = fs::read_to_string(config_path)?;
-        let config_file: ConfigFile = toml::from_str(&contents)?;
-        Ok(Self {
-            charge_start_threshold: Some(config_file.battery.start_threshold),
-            charge_stop_threshold: Some(config_file.battery.stop_threshold),
-        })
+  pub fn parse(config_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+    if !Path::new(config_path).exists() {
+      return Err(Box::new(Error::new(
+        ErrorKind::NotFound,
+        "No config file found",
+      )));
     }
 
-    pub fn apply(&self, system_state: &SystemState) -> Result<(), SystemStateError> {
-        if system_state.acpi_type != ACPIType::ThinkPad {
-            return Err(SystemStateError::ACPITypeErr(
-                "only thinkpad acpi supported for now".to_string(),
-            ));
-        }
+    let contents = fs::read_to_string(config_path)?;
+    let config_file: ConfigFile = toml::from_str(&contents)?;
+    Ok(Self {
+      charge_start_threshold: Some(config_file.battery.start_threshold),
+      charge_stop_threshold: Some(config_file.battery.stop_threshold),
+    })
+  }
 
-        if let Some(start_thresh) = self.charge_start_threshold {
-            println!("Setting charge start threshold to {}", start_thresh);
-            system_state
-                .battery_states
-                .set_charge_start_threshold(start_thresh.into())?;
-        }
-
-        if let Some(stop_thresh) = self.charge_stop_threshold {
-            println!("Setting charge stop threshold to {}", stop_thresh);
-            system_state
-                .battery_states
-                .set_charge_stop_threshold(stop_thresh.into())?;
-        }
-
-        Ok(())
+  pub fn apply(&self, system_state: &SystemState) -> Result<(), SystemStateError> {
+    if system_state.acpi_type != ACPIType::ThinkPad {
+      return Err(SystemStateError::ACPITypeErr(
+        "only thinkpad acpi supported for now".to_string(),
+      ));
     }
 
-    pub fn get_config_path() -> Result<String, env::VarError> {
-        if let Ok(sudo_user) = env::var("SUDO_USER") {
-            Ok(format!("/home/{}/.config/powereg/config.toml", sudo_user))
-        } else {
-            let home = env::var("HOME")?;
-            Ok(format!("{}/.config/powereg/config.toml", home))
-        }
+    if let Some(start_thresh) = self.charge_start_threshold {
+      println!("Setting charge start threshold to {}", start_thresh);
+      system_state
+        .battery_states
+        .set_charge_start_threshold(start_thresh.into())?;
     }
 
-    pub fn setup_config(system_state: &SystemState) {
-        if let Ok(config_path) = Config::get_config_path() {
-            println!("Config path: {config_path}");
-            match Config::parse(&config_path) {
-                Ok(config) => match config.apply(&system_state) {
-                    Ok(_) => {}
-                    Err(e) => println!("{} {}", "Error while applying config:".red(), e),
-                },
-                Err(e) => eprintln!("{} {}", "Error loading config:".red(), e),
-            };
-        } else {
-            println!("{}", "Error loading config".red());
-        }
+    if let Some(stop_thresh) = self.charge_stop_threshold {
+      println!("Setting charge stop threshold to {}", stop_thresh);
+      system_state
+        .battery_states
+        .set_charge_stop_threshold(stop_thresh.into())?;
     }
+
+    Ok(())
+  }
+
+  pub fn get_config_path() -> Result<String, env::VarError> {
+    if let Ok(sudo_user) = env::var("SUDO_USER") {
+      Ok(format!("/home/{}/.config/powereg/config.toml", sudo_user))
+    } else {
+      let home = env::var("HOME")?;
+      Ok(format!("{}/.config/powereg/config.toml", home))
+    }
+  }
+
+  pub fn setup_config(system_state: &SystemState) {
+    if let Ok(config_path) = Config::get_config_path() {
+      println!("Config path: {config_path}");
+      match Config::parse(&config_path) {
+        Ok(config) => match config.apply(&system_state) {
+          Ok(_) => {}
+          Err(e) => println!("{} {}", "Error while applying config:".red(), e),
+        },
+        Err(e) => eprintln!("{} {}", "Error loading config:".red(), e),
+      };
+    } else {
+      println!("{}", "Error loading config".red());
+    }
+  }
 }
